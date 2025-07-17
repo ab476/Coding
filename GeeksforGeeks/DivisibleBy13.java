e8bd6f63898ff239e46015fe44eb48da4c8b0482
Arijit Biswas <arijitbiswas476@gmail.com>
2025-07-17 22:50:53 +0530

    new file:   GeeksforGeeks/DivisibleBy13.java

---
+++ GeeksforGeeks/DivisibleBy13.java
@@ -0,0 +1,9 @@
+public class DivisibleBy13 {
+    public boolean divby13(String s) {
+       int rem = 0;
+       for(int i = 0; i < s.length(); i++){
+            rem = (rem * 10 + (s.charAt(i) - '0')) % 13;
+       }
+       return rem == 0;
+    }
+}
