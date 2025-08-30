393ef0878f825f1532a5ca5eb62c98e80fb7cc21
Arijit Biswas <arijitbiswas476@gmail.com>
2025-08-30 19:56:19 +0530

    new file:   GeeksforGeeks/GFG160/Arrays/ReverseArray.java

---
+++ GeeksforGeeks/GFG160/Arrays/ReverseArray.java
@@ -0,0 +1,14 @@
+package GeeksforGeeks.GFG160.Arrays;
+
+public class ReverseArray {
+    public void reverseArray(int arr[]) {
+        // code here
+        int mid = (arr.length / 2) - 1;
+        int last = arr.length - 1;
+        for (int i = 0; i <= mid; i++) {
+            int temp = arr[last - i];
+            arr[last - i] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
